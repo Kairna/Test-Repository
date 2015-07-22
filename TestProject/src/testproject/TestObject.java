e5f12d7e1a55cbf651dcc15e1ed05c89799367c5
Kai Salamanca <kmgsalamanca@gmail.com>
2015-07-22 23:15:46 +0800

    TestObject
    TestObject

---
+++ TestProject/src/testproject/TestObject.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testproject;
+
+/**
+ *
+ * @author ksalaman
+ */
+public class TestObject {
+    
+    int x=0;
+    int y=0;
+    int z=0;
+    public TestObject(int x, int y, int z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        
+    }
+    
+}
